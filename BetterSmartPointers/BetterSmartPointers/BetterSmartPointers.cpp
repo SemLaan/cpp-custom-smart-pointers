245416aedae9e0435ec280abaf6fcd7b319ccad3
semlaan <semlaan@dds.nl>
2023-08-09 15:59:29 +0200

    Simple scope pointer that is a less complete version of std::unique_ptr

--- BetterSmartPointers/BetterSmartPointers/BetterSmartPointers.cpp
+++ BetterSmartPointers/BetterSmartPointers/BetterSmartPointers.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include "example_class.h"
+#include "scope.h"
 
 #define MEMORY_LEAK_TEST_COUNT 10000
 
 
 int main()
 {
+    // === Scope ptr ===
+    {
+        Scope<int> uniq = CreateScope<int>(3);
+        *uniq = 5;
+
+        Scope<Example> example = CreateScope<Example>(3);
+        example->PrintVars();
+
+        for (int i = 0; i < MEMORY_LEAK_TEST_COUNT; ++i)
+        {
+            // Example has a pointer in it so if the destructor doesn't get called properly this will create a leak
+            // If you are in debug mode ofcourse because this will get optimized out in release because this for loop does nothing
+            Scope<Example> temp = CreateScope<Example>();
+        }
+    }
+
     // === Unique ptr ===
     {
         std::unique_ptr<int> uniq = std::make_unique<int>(3);
---
+++ BetterSmartPointers/BetterSmartPointers/scope.h
@@ -0,0 +1,52 @@
+#pragma once
+
+
+template <typename T>
+class Scope
+{
+private:
+	T* m_ptr;
+public:
+	// Default constructor
+	Scope()
+		: m_ptr(nullptr)
+	{}
+
+	// Constructor from pointer, this takes ownership over the pointer
+	explicit Scope(T* ptr)
+		: m_ptr(ptr)
+	{}
+
+	~Scope()
+	{
+		delete m_ptr;
+	}
+
+	// Dereference operator
+	T& operator*() const
+	{
+		return *m_ptr;
+	}
+
+	// Arrow operator
+	T* operator->() const
+	{
+		// This returns the pointer because I think you're not actually overloading the true arrow operator, so when you
+		// call the arrow operator on the scope "scopeObj->SomeFunc()" the arrow operator first returns the pointer -- which is what this code does --
+		// and then it applies the "global" arrow operator that dereferences the pointer
+		// If you simply return the dereferenced pointer it gives a vague warning about possible recursion
+		return m_ptr;
+	}
+};
+
+// Function for creating a scope of object type T with parameters args of types Types
+// This is so unreadable because we want to avoid copying the args, so we have to first take the args as rvalues,
+// and then because we're passing them to another function we have to let the compiler(?) know that the constructor of T can take
+// ownership of them as if they are still rvalues by using std::forward
+// We also don't know the type of scope nor the constructor arguments that takes so we have to use templates
+// And finally the ... are for variadic arguments
+template <typename T, typename... Types>
+Scope<T> CreateScope(Types&&... args)
+{
+	return Scope<T>(new T(std::forward<Types>(args)...));
+}
